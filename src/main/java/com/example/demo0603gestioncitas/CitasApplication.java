a8e443c458feb17a454818601de53f7c6da2a5f5
Javier Roldan <javier.roldan@babelgroup.com>
2023-03-06 11:53:12 +0100

    first commit

---
+++ src/main/java/com/example/demo0603gestioncitas/CitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo0603gestioncitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasApplication.class, args);
+	}
+
+}
